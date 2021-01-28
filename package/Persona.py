c9e09df0241b6f9c38fb8bacc48bdf3c64c2554a
Javi <javimenendezg@gmail.com>
2021-01-28 11:34:09 +0100

    commit: mejora

--- package/Persona.py
+++ package/Persona.py
@@ -1,21 +1,25 @@
-# from datetime import *
+from datetime import *
 
 
 class Persona(object):
     def __init__(self,nombre:str,apellido:str,fecha:str,dni:str,direccion:list):
         self.nombre=nombre
         self.apellido=apellido
-        self.fecha=fecha
-        # self.fecha=datetime.strptime(fecha,"d%/m%/Y%").date()
+        self.fecha=datetime.strptime(fecha,"%d/%m/%Y")
         self.dni=dni
         self.direccion=direccion
     def __str__(self):
-        return f"nombre: {self.nombre}, apellido: {self.apellido}, fecha: {self.fecha}, dni: {self.dni}, direccion: {self.direccion}"
+        fechastr=self.fecha.strftime("%d/%m/%Y")
+        return f"nombre: {self.nombre}, apellido: {self.apellido}, fecha: {fechastr}, dni: {self.dni}, direccion: {self.direccion}"
     def getNombreCompleto(self):
         return self.nombre+" "+self.apellido
 
 javi1=Persona("javi","menendez","19/06/1998","123456789F",["Narvaez","51","4º","Izq"])
 print(javi1)
 print(javi1.getNombreCompleto())
+
+
 # print("19/06/1998")
-# print(datetime.strptime(r"19/06/1998",r"d%/m%/Y%"))
+# hola=datetime.strptime("19/06/1998", "%d/%m/%Y")
+# print(hola)
+# print(hola.strftime("%d/%m/%Y"))
