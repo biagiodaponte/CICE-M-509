9a5fd92796905215d1b06e2dfa0cc2063c9fb9f0
“VeronicaSH” <veronicasalguero.95@gmail.com>
2021-01-28 10:57:59 +0100

    1

--- package/persona.py
+++ package/persona.py
@@ -1,20 +1,22 @@
 import datetime
 
-
-nombre = ""
-apellido = ""
-fecha_nacimiento = datetime.date
-dni = ""
-direccion = "" 
-
-#imprimir nombre 
-def getNombreCompleto(self):
+class Persona(object):
+    #CONSTRUCTOR
+    def __init__(self,nombre:str,apellido:str,fecha_nacimiento:datetime,dni:str,direccion:str)
     self.nombre = nombre
     self.apellido = apellido
-    res = nombre + apellido
-    print(res)
+    self.fecha_nacimiento = fecha_nacimiento
+    self.dni = dni
+    self.direccion = direccion  
 
-def fecha(self,dia,mes,anio):
-    nueva = datetime.date(anio,mes,dia)
-    print(nueva)
+    #imprimir nombre 
+    def getNombreCompleto(self):
+        self.nombre = nombre
+        self.apellido = apellido
+        res = nombre + apellido
+        print(res)
+
+    def fecha(self,dia,mes,anio):
+        nueva = datetime.date(anio,mes,dia)
+        print(nueva)
 
--- package/usuario.py
+++ package/usuario.py
@@ -1,14 +1,19 @@
-email = ""
-clave = ""
-activo = False
 
-def validacion(self, param_email, param_clave):
-    self.email = email
-    self.clave = clave 
-    if(email == param_email and clave == param_clave):
-        valido = True
-        self.activo = True
-    else:
-        valido = False
-        
-    return valido
+class Usuario(object):
+    #CONSTRUCTOR
+    def __init__(self, email:str,clave:str,activo:bool)
+        self.email = email 
+        self.clave = clave 
+        self.activo = activo
+
+    def validacion(self, param_email, param_clave):
+        self.email = email
+        self.clave = clave 
+        activo = False
+        if(email == param_email and clave == param_clave):
+            valido = True
+            self.activo = True
+        else:
+            valido = False
+            
+        return valido
