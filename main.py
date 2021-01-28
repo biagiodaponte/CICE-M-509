91092d48f72716efd93a808fa90a462f85624aa1
Isabel <isargp@gmail.com>
2021-01-28 11:16:36 +0100

    clase usuario creada (woorking)

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 # EJEMPLO DE MANEJO  DE REPOSITORIOS Y CLASES POR MEDIO DE PAQUETES
 from package.persona import Persona
+from package.usuario import Usuario
 
 def main():
-    yo = Persona('Isabel', 'Repetto', '12-02-1991', '56432865P', "Tailor's Court")
+    # yo = Persona('Isabel', 'Repetto', '12-02-1991', '56432865P', "Tailor's Court")
 
-    print(yo.getDia())
-    print(yo.getMes())
-    print(yo.getAño())
-    yo.setDia(22)
-    print(yo)
+    # print(yo.getDia())
+    # print(yo.getMes())
+    # print(yo.getAño())
+    # yo.setDia(22)
+    # print(yo)
+
+    yo = Usuario('isargp@gmail.com', 'clavecita', False)
+    print(yo.validacion('isargp@gmail.com', 'clavecita'))
 
 main()
