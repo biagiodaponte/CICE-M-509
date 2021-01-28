ecd8346fa1d5ff71b931995ae29511d6baca2fbe
“VeronicaSH” <veronicasalguero.95@gmail.com>
2021-01-28 10:08:07 +0100

    3

--- package/persona.py
+++ package/persona.py
@@ -1,5 +1,20 @@
-nombre 
-apellido
-fecha_nacimiento
-dni 
-direccion 
+import datetime
+
+
+nombre = ""
+apellido = ""
+fecha_nacimiento = datetime.date
+dni = ""
+direccion = "" 
+
+#imprimir nombre 
+def getNombreCompleto(self):
+    self.nombre = nombre
+    self.apellido = apellido
+    res = nombre + apellido
+    print(res)
+
+def fecha(self,dia,mes,anio):
+    nueva = datetime.date(anio,mes,dia)
+    print(nueva)
+
---
+++ package/usuario.py
@@ -0,0 +1,3 @@
+email = ""
+clave = ""
+activo = False
