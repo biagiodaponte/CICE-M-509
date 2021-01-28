d470ebd071f4ab68b2cb1962ae75b148cce0432c
Ciriaco <ta01001011@gmail.com>
2021-01-28 10:17:34 +0100

    fifth commit

---
+++ package/user.py
@@ -0,0 +1,52 @@
+
+
+
+#5.- Create a class User with the next attributes:
+#    email: str
+#    key: str
+#    active: bool
+
+
+class User:
+    active = False
+    
+    def __init__(self, email, key):
+        self.email = email
+        self.key = key
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
