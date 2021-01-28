6166d008a467a607008111ba0c0d67eb6f627d47
“VeronicaSH” <veronicasalguero.95@gmail.com>
2021-01-28 10:21:19 +0100

    Update usuario.py

--- package/usuario.py
+++ package/usuario.py
@@ -1,3 +1,14 @@
 email = ""
 clave = ""
 activo = False
+
+def validacion(self, param_email, param_clave):
+    self.email = email
+    self.clave = clave 
+    if(email == param_email and clave == param_clave):
+        valido = True
+        self.activo = True
+    else:
+        valido = False
+        
+    return valido
