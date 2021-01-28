15e09a0c5f2f27e3585b3d7d229ff5ad112201dd
evander <lopezbilbaoevander@gmail.com>
2021-01-28 11:53:37 +0100

    commit_1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
 # EJEMPLO DE MANEJO  DE REPOSITORIOS Y CLASES POR MEDIO DE PAQUETES
+from package.persona import Persona
 
 def main():
-    pass
+    objeto = Persona( 'Evander', 'Lopez', "00055m", "19-7-1992","povedilla")
+    
 
+    objeto.getDia()
+    print(objeto.getDia())
+    print(objeto.getMes())
+    print(objeto.getAño())
 main()
--- package/persona.py
+++ package/persona.py
@@ -1,16 +1,64 @@
 
 
 class Persona:
+
+    
     def __init__(self , nombre:str,apellido:str,dni:str,fecha_de_nacimiento:str,direccion:list):
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni 
         self.fecha_de_nacimiento = fecha_de_nacimiento
         self.direccion = direccion
+      
 
-    def __str__(self):
-        return f'{self.nombre} {self.apellido}'
+    def getnombreapellido(self):
+        return f"{self.nombre} {self.apellido}"
 
 
-nombreapellido = Persona( 'Evander', 'Lopez', "00055m", "1-5-1850","povedilla")
-print(nombreapellido)
+    def getDia(self):
+        fecha = self.fecha_de_nacimiento.split('-')
+        return fecha[0]
+
+    def getMes(self):
+        fecha = self.fecha_de_nacimiento.split('-')
+        return fecha[1]
+
+    def getAño(self):
+        fecha = self.fecha_de_nacimiento.split('-')
+        return fecha[2]
+
+
+
+
+
+    def setDia(self, dia):
+        fecha = self.fecha_de_nacimiento.split("-")
+        fecha[0]= dia 
+        self.fecha_de_nacimiento = fecha[0]+'-'+fecha[1]+'-'+fecha[2]
+
+    def setMes(self, mes):
+        fecha = self.fecha_de_nacimiento.split("-")
+        fecha[1]= mes
+        self.fecha_de_nacimiento = fecha[0] + "-" + fecha[1] + "-" +fecha[2]
+         
+    def  setAño(self, año):
+        fecha = self.fecha_de_nacimiento.split("-")
+        fecha[2]= año
+        self.fecha_de_nacimiento = fecha[0] + "-" + fecha[1] + "-" + fecha[2]
+
+
+
+
+
+
+
+
+
+
+
+
+    objeto = Persona( 'Evander', 'Lopez', "00055m", "19-7-1992","povedilla")
+    objeto.setDia()
+    print(objeto.setDia())
+    print(objeto.setMes())
+    print(objeto.setAño())
