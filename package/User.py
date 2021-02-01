ef06cc6aa92ba3724bb7094e644bb28109068580
Javi <javimenendezg@gmail.com>
2021-02-01 09:21:44 +0100

    commit: ejercicio 6

--- package/User.py
+++ package/User.py
@@ -10,6 +10,11 @@ class User (object):
         return f"email: {self.email}, clave: {self.clave}, activo: {activostr}"
 
     def validacion(self, param_email, param_clave):
-        para
+        if param_email==self.email and param_clave==self.clave and self.activo==True:
+            return True
+        return False
+
 user1=User("qwerty@asdf","123456",True)
 print(user1)
+print(user1.validacion("aaaaa","123456"))
+print(user1.validacion("qwerty@asdf","123456"))
