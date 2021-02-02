033f4a66843f2cf58acba3ae8574e32e68416baf
javi <javimenendezg@gmail.com>
2021-02-02 11:20:02 +0100

    commit: pruebas

---
+++ errores.py
@@ -0,0 +1,14 @@
+
+
+# class EjemploDeError(KeyboardInterrupt):
+
+# def myfunction(edad):
+# try:
+#     edad = int(edad)
+#     if edad>18:
+#         return "eres adulto"
+
+#     else:
+#         return "eres joven"
+# except ValueError:
+#     print("debes ingresar un numero")
